810484c50f0a1a0447e7319640e2350be5f19404
Rock Kyun <mjnn100801@naver.com>
2025-02-06 21:59:50 +0900

    fix(chatBox, chatInput): 채팅 입력 시 스크롤이 하단으로 이동되도록, 채팅 POST 시 userId가 동적으로 할당되도록

--- src/components/chat/ChatBox.tsx
+++ src/components/chat/ChatBox.tsx
@@ -1,6 +1,7 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import ChatInput from "./ChatInput";
 import { supabase } from "../../api/supabase";
+import { useAuth } from "../../provider/AuthProvider";
 
 interface ChatboxProps {
   roomId: number;
@@ -13,6 +14,8 @@ interface Chats {
 }
 
 const ChatBox: React.FC<ChatboxProps> = ({ roomId }) => {
+  const { user } = useAuth();
+  const messageEndRef = useRef<HTMLDivElement | null>(null);
   const [messages, setMessages] = useState<Chats[]>([]);
 
   // 방에 입장 했을 때, 메세지 가져오기
@@ -77,14 +80,28 @@ const ChatBox: React.FC<ChatboxProps> = ({ roomId }) => {
     };
   }, [roomId]);
 
+  useEffect(() => {
+    messageEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  });
+
   return (
     <div className="grid grid-rows-3 w-3xs h-96 m-4 border rounded-sm p-3">
       <p>Room Name</p>
       <div className="overflow-scroll">
-        {messages.map((message, index) => (
-          <p key={index}>{message.messages}</p>
-        ))}
+        {messages.map((message, index) =>
+          message.userId === user?.id ? (
+            <p className="text-right" key={index}>
+              {message.messages}
+            </p>
+          ) : (
+            <p className="text-left" key={index}>
+              {message.messages}
+            </p>
+          )
+        )}
+        <div className="h-[0.1rem]" ref={messageEndRef}></div>
       </div>
+
       <div>
         <ChatInput sendMessage={sendMessageHandler} />
       </div>
