65e3fb3149642cb520340295fbc4875b74383baa
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2025-02-09 22:10:16 +0900

    refactor: Move game action buttons to GameButtonWrapper component

--- src/components/game/GameButtonWrapper.tsx
+++ src/components/game/GameButtonWrapper.tsx
@@ -1,12 +1,21 @@
 import useGame from "../../hooks/game/useGame";
+import { canTurnEnd } from "../../modules/Game";
 
 const GameButtonWrapper = () => {
-  const { gameState, handleStartGame } = useGame();
+  const { gameState, handleStartGame, handlePlay, handleTurnEnd } = useGame();
 
   const showStartBtn = gameState === null;
 
   return (
     <div>
+      {gameState && (
+        <>
+          <button onClick={handlePlay}>제출</button>
+          <button onClick={handleTurnEnd} disabled={!canTurnEnd(gameState!)}>
+            턴넘기기
+          </button>
+        </>
+      )}
       {showStartBtn && <button onClick={handleStartGame}>시작하기</button>}
     </div>
   );
