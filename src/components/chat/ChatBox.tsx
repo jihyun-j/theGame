44e64063b87f0f9cd51431058b9944b145bc555a
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2025-02-17 21:53:45 +0900

    refactor: Simplify ChatBox component using Zustand room store

--- src/components/chat/ChatBox.tsx
+++ src/components/chat/ChatBox.tsx
@@ -1,6 +1,6 @@
-import { useEffect, useState } from "react";
 import { supabase } from "../../api/supabase";
 import { getNextChat } from "../../modules/Chat/chat";
+import useRoom from "../../store/room.store";
 import { Chat } from "../../types/types";
 import ChatInput from "./ChatInput";
 
@@ -9,20 +9,8 @@ interface ChatboxProps {
 }
 
 const ChatBox: React.FC<ChatboxProps> = ({ roomId }) => {
-  const [messages, setMessages] = useState<Chat[]>([]);
-
-  // 방에 입장 했을 때, 메세지 가져오기
-  const fetchMessage = async () => {
-    const { data, error } = await supabase
-      .from("rooms")
-      .select("chats")
-      .eq("id", roomId)
-      .single();
-
-    if (!error && data.chats) {
-      setMessages((data.chats as Chat[]) || []);
-    }
-  };
+  const { room } = useRoom();
+  const messages = room?.chats as Chat[];
 
   // 메세지 보내기
   const sendMessageHandler = async (text: string, userId: string) => {
@@ -52,35 +40,11 @@ const ChatBox: React.FC<ChatboxProps> = ({ roomId }) => {
       .eq("id", roomId);
   };
 
-  useEffect(() => {
-    fetchMessage();
-
-    const subscription = supabase
-      .channel(`game-room-${roomId}`)
-      .on(
-        "postgres_changes",
-        {
-          event: "UPDATE",
-          schema: "public",
-          table: "rooms",
-          filter: `id=eq.${roomId}`,
-        },
-        (payload) => {
-          setMessages(payload.new.chats || []);
-        },
-      )
-      .subscribe();
-
-    return () => {
-      supabase.removeChannel(subscription);
-    };
-  }, [roomId]);
-
   return (
     <div className='grid grid-rows-3 w-3xs h-96 m-4 border rounded-sm p-3'>
       <p>Room Name</p>
       <div className='overflow-scroll'>
-        {messages.map((message, index) => (
+        {messages?.map((message, index) => (
           <p key={index}>{message.msg}</p>
         ))}
       </div>
